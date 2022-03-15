0bd26fe8a5c878cc49c422ae09dc2f9077ad97b9
SalauOyin <salauoyinkan@gmail.com>
2022-03-15 15:31:05 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@
 
 void jack_bauer(void)
 {
-	int hours_tens, hours_tens, minutes_hours, minutes_tens, minutes_ones, hours_max;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
 	hours_max = 58;
 	hours_tens = '0';
@@ -26,26 +26,27 @@ void jack_bauer(void)
 			minutes_tens = '0';
 			while (minutes_tens < '6')
 			{
-			minutes_ones = '0';
-			while (minutes_ones < 58)
-			{
-				_putchar(hours_tens);
-				_putchar(hours_ones);
-				_putchar(':');
-				_putchar(minutes_tens);
-				_putchar(minutes_ones);
-				_putchar('\n');
-				minutes_ones++;
-			}
-			minutes_ones = '0';
-			minutes_tens++;
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{	
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+
+			}	
+			minutes_tens = '0';
+			hours_ones++;
 
 		}
-		minutes_tens = '0';
-		hours_ones++;
+		hours_ones = '0';
+		hours_tens++;
+	}
 
-	}
-	hours_ones = '0';
-	hours_tens++;
-	}
 }
