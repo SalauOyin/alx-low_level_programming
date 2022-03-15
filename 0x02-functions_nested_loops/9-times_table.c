09ae4b914769406d58b2eecf41c61d37ca23c2ed
SalauOyin <salauoyinkan@gmail.com>
2022-03-15 15:57:21 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * times_table - prints the 9 times table
- * 
+ *
  * Description: prints the 9 times table
  *
  * Return: void
@@ -19,7 +19,7 @@ void times_table(void)
 			product = row * column;
 			tens = product / 10;
 			ones = product % 10;
-		
+
 			if (column == 0)
 			{
 				_putchar('0');
