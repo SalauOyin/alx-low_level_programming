d57e1aa0feaca55283683fb55e029b8e00113a36
SalauOyin <80987417+SalauOyin@users.noreply.github.com>
2022-03-21 21:00:37 +0100

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,17 +3,10 @@
 /**
  * reset_to_98 - takes a pointer and updates the value it point to
  * @n: pointer
- *
- *    * Return: void
- *
- *     */
-
-
+ * Return: void
+ */
 
 void reset_to_98(int *n)
-
 {
-
-		*n = 98;
-
+	*n = 98;
 }
